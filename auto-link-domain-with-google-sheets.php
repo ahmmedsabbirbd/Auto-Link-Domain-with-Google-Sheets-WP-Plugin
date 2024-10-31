fecca0597d3caa3b5638cb10c9c1d810e963ce26
ahmmedsabbirbd <ahmmedsabbirbd@gmail.com>
2024-10-31 12:22:09 +0600

    added one function & sheet_id update

--- auto-link-domain-with-google-sheets.php
+++ auto-link-domain-with-google-sheets.php
@@ -16,10 +16,17 @@ register_activation_hook(__FILE__, 'demo_with_google_sheets_activate_osgs');
 
 add_action('admin_notices', 'show_current_domain_notice_osgs');
 
+function extract_sheet_id($url) {
+    // PHP version of extracting Google Sheets ID
+    if (preg_match('/\/d\/([a-zA-Z0-9-_]+)/', $url, $matches)) {
+        return $matches[1];
+    }
+    return "Invalid Google Sheets link";
+}
 
 function show_current_domain_notice_osgs() {
     $current_domain = home_url();
-    $myCurrentDomain = "https://complexplough.s4-tastewp.com";
+    $myCurrentDomain = "http://localhost/osgs-demo-site";
 
     if($current_domain !== $myCurrentDomain) {
         $demo_one_time_load_osgs = get_option('demo_one_time_load_osgs');
@@ -39,7 +46,10 @@ function show_current_domain_notice_osgs() {
             $body = wp_remote_retrieve_body($response);
             $data = json_decode($body, true);
 
+
+
             update_option('osgsw_spreadsheet_url', $data["sheet_url"]);
+            update_option('osgsw_spreadsheet_id', extract_sheet_id($data["sheet_url"]));
             // Trigger the sync action
             do_action('osgsw_updated_save_and_sync');
             update_option('demo_one_time_load_osgs', true);
